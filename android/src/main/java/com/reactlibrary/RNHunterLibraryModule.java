6cb405f3e0464b39acae7b398cb7bacc5d597444
Hunter <hunter.pu8@gmail.com>
2018-11-12 11:54:25 +0800

    add toast

--- android/src/main/java/com/reactlibrary/RNHunterLibraryModule.java
+++ android/src/main/java/com/reactlibrary/RNHunterLibraryModule.java
@@ -7,6 +7,8 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
 public class RNHunterLibraryModule extends ReactContextBaseJavaModule {
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
 
   private final ReactApplicationContext reactContext;
 
@@ -18,5 +20,18 @@ public class RNHunterLibraryModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNHunterLibrary";
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
+  }
+
+  @ReactMethod
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
 }
